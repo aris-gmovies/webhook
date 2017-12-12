566b226f4ab5433d4a1a1176fd095f60e787337f
Aris <aespiritu@yondu.com>
2017-12-12 14:22:43 +0800

    Updates

--- index.js
+++ index.js
@@ -100,6 +100,23 @@ body.entry.forEach(function(entry) {
   
 });
 
+function handleMessage(sender_psid, received_message) {
+
+  let response;
+
+  // Check if the message contains text
+  if (received_message.text) {    
+
+    // Create the payload for a basic text message
+    response = {
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+    }
+  }  
+  
+  // Sends the response message
+  callSendAPI(sender_psid, response);    
+}
+
 function callSendAPI(sender_psid, response) {
   // Construct the message body
   let request_body = {
