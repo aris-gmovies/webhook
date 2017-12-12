dce664d41b724d8cabe7532955e2562d085272df
Aris <aespiritu@yondu.com>
2017-12-12 14:45:21 +0800

    update

--- index.js
+++ index.js
@@ -11,26 +11,25 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {  
-
-  // Parse the request body from the POST
+ 
   let body = req.body;
 
-  // Check the webhook event is from a Page subscription
+  // Checks this is an event from a page subscription
   if (body.object === 'page') {
 
-    // Iterate over each entry - there may be multiple if batched
+    // Iterates over each entry - there may be multiple if batched
     body.entry.forEach(function(entry) {
 
-  // Gets the body of the webhook event
-  let webhook_event = entry.messaging[0];
-  console.log(webhook_event);
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhookEvent = entry.messaging[0];
+      console.log(webhookEvent);
+    });
 
-  // Get the sender PSID
-  let sender_psid = webhook_event.sender.id;
-  console.log('Sender PSID: ' + sender_psid);
-
-  }  else {
-    // Return a '404 Not Found' if event is not from a page subscription
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
   }
 
@@ -65,20 +64,3 @@ app.get('/webhook', (req, res) => {
 });
 
 
-
-
-
-// Handles messages events
-function handleMessage(sender_psid, received_message) {
-
-}
-
-// Handles messaging_postbacks events
-function handlePostback(sender_psid, received_postback) {
-
-}
-
-// Sends response messages via the Send API
-function callSendAPI(sender_psid, response) {
-  
-}
