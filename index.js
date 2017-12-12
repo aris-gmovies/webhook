fb0d000dd3be63e76de26e9190356487e5c54d92
Aris <aespiritu@yondu.com>
2017-12-12 14:21:04 +0800

    Updates

--- index.js
+++ index.js
@@ -110,15 +110,6 @@ function callSendAPI(sender_psid, response) {
   }
 }
 
-function callSendAPI(sender_psid, response) {
-  // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
-
   // Send the HTTP request to the Messenger Platform
   request({
     "uri": "https://graph.facebook.com/v2.6/me/messages",
