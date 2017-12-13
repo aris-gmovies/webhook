b53948a07bc546d53a14a7d2e9b02b783a5b2b64
Aris <aespiritu@yondu.com>
2017-12-13 17:53:34 +0800

    Updates

--- index.js
+++ index.js
@@ -85,7 +85,42 @@
      function sendGenericMessage(sender) {
       let messageData = {
             "text": "Would you like to play a game?",
-            
+            "attachments": [
+                {
+                    "text": "Choose a game to play",
+                    "fallback": "You are unable to choose a game",
+                    "callback_id": "wopr_game",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "game",
+                            "text": "Chess",
+                            "type": "button",
+                            "value": "chess"
+                        },
+                        {
+                            "name": "game",
+                            "text": "Falken's Maze",
+                            "type": "button",
+                            "value": "maze"
+                        },
+                        {
+                            "name": "game",
+                            "text": "Thermonuclear War",
+                            "style": "danger",
+                            "type": "button",
+                            "value": "war",
+                            "confirm": {
+                                "title": "Are you sure?",
+                                "text": "Wouldn't you prefer a good game of chess?",
+                                "ok_text": "Yes",
+                                "dismiss_text": "No"
+                            }
+                        }
+                    ]
+                }
+            ]
         }
       request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
