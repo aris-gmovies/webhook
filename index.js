8a2ecc0d1c0978d6d64e9f6f10dd480a9edde06d
Aris <aespiritu@yondu.com>
2017-12-13 10:37:48 +0800

    Updates

--- index.js
+++ index.js
@@ -1,32 +1,107 @@
-'use strict'
-    
-    const express = require('express')
-    const bodyParser = require('body-parser')
-    const request = require('request')
-    const app = express()
+'use strict';
 
-    app.set('port', (process.env.PORT || 5000))
+// Imports dependencies and set up http server
+const 
+  express = require('express'),
+  bodyParser = require('body-parser'),
+  app = express().use(bodyParser.json()); // creates express http server
 
-    // Process application/x-www-form-urlencoded
-    app.use(bodyParser.urlencoded({extended: false}))
+// Sets server port and logs message on success
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+port: process.env.PORT
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+  // Make sure this is a page subscription
+  console.log('aw')
+  if (req.body.object == "page") {
+    // Iterate over each entry
+    // There may be multiple entries if batched
+    req.body.entry.forEach(function(entry) {
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.postback) {
+          processPostback(event);
+        }
+      });
+    });
 
-    // Process application/json
-    app.use(bodyParser.json())
+    res.sendStatus(200);
+  }
+});
 
-    // Index route
-    app.get('/', function (req, res) {
-      res.send('Hello world, I am a chat bot')
-    })
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
 
-    // for Facebook verification
-    app.get('/webhook/', function (req, res) {
-      if (req.query['hub.verify_token'] === 'gmovies-webhook') {
-        res.send(req.query['hub.challenge'])
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: 'EAAJVjSGHUrYBAOJx0KK2B9gPudRguiZBT8zuFZBsjCGFLTn8nM4nRQ49XGXVTb50ZA60qRfXQVSkdFq5AVV7OAANHhcbAVdYd1ly9ZA7qiUKZAZCtRMOzY6RylpbsOjPugcjbZAt0yhcxFSh3ZCkiz9KNQ7kwaZAzZBGJApk1956bxZAAZDZD',
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
       }
-      res.send('Error, wrong token')
-    })
+      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
+  }
+}
 
-    // Spin up the server
-    app.listen(app.get('port'), function() {
-      console.log('running on port', app.get('port'))
-    })
+// sends message to user
+function sendMessage(recipientId, message) {
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: 'EAAJVjSGHUrYBAOJx0KK2B9gPudRguiZBT8zuFZBsjCGFLTn8nM4nRQ49XGXVTb50ZA60qRfXQVSkdFq5AVV7OAANHhcbAVdYd1ly9ZA7qiUKZAZCtRMOzY6RylpbsOjPugcjbZAt0yhcxFSh3ZCkiz9KNQ7kwaZAzZBGJApk1956bxZAAZDZD'},
+    method: "POST",
+    json: {
+      recipient: {id: recipientId},
+      message: message,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log("Error sending message: " + response.error);
+    }
+  });
+}
+
+
+// Adds support for GET requests to our webhook
+app.get('/webhook', (req, res) => {
+
+  // Your verify token. Should be a random string.
+  let VERIFY_TOKEN = "gmovies-webhook"
+    
+  // Parse the query params
+  let mode = req.query['hub.mode'];
+  let token = req.query['hub.verify_token'];
+  let challenge = req.query['hub.challenge'];
+    
+  // Checks if a token and mode is in the query string of the request
+  if (mode && token) {
+  
+    // Checks the mode and token sent is correct
+    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+      
+      // Responds with the challenge token from the request
+      console.log('WEBHOOK_VERIFIED');
+      res.status(200).send(challenge);
+    
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);      
+    }
+  }
+});
+
+
