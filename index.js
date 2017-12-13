627fdd0c83caaf4cf33d944a53a3dda26f4ce115
Aris <aespiritu@yondu.com>
2017-12-13 15:09:31 +0800

    Updates

--- index.js
+++ index.js
@@ -84,10 +84,32 @@
 
      function sendGenericMessage(sender) {
       let messageData = {
-        {
-           text: "This is a line of text.\nAnd this is another one."
+        "attachment": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "buttons": [{
+                "type": "web_url",
+                "url": "https://www.messenger.com",
+                "title": "web url"
+              }, {
+                "type": "postback",
+                "title": "Postback",
+                "payload": "Payload for first element in a generic bubble",
+              }],
+            }, {
+              "title": "SCHEDULES",
+              "subtitle": "Element #2 of an hscroll",
+              "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
+              "buttons": [{
+                "type": "postback",
+                "title": "Postback",
+                "payload": "Payload for second element in a generic bubble",
+              }],
+            }]
+          }
         }
-        
       }
       request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
